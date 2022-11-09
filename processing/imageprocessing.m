24f78524ca9a5523526bcd9da3dfa95a8fdb1b3b
yezehao <sgzye5@liverpool.ac.uk>
2022-11-09 12:10:02 +0000

    modify the image processing code

--- processing/imageprocessing.m
+++ processing/imageprocessing.m
@@ -15,17 +15,12 @@ subplot(1,3,3);imshow(pro);
 
 %% Plot
 figure,imshow(obj);
-    for v=1:num
-        [r, c]=find(labelpic==v);
-        [rectx,recty,area,perimeter]=minboundrect(c,r,'p');
-        [length, width] = minboxing(rectx(1:end-1),recty(1:end-1));
-         % draw the rectangle
-        line(rectx,recty,'color','y','linewidth',2);
-        midpointx(1)=((rectx(1)+rectx(2))/2);
-        midpointx(3)=(rectx(2)+rectx(3))/2;
-        midpointy(1)=(recty(1)+recty(2))/2;
-        midpointy(3)=(recty(2)+recty(3))/2;
-    end
-    path = 'D:\research\final-year-project\result.jpg';
-    saveas(gcf,path)
-    close;
+for v=1:num
+    [r, c]=find(labelpic==v);
+    [rectx,recty,area,perimeter]=minboundrect(c,r,'p');
+    [length, width] = minboxing(rectx(1:end-1),recty(1:end-1));
+    % draw the rectangle
+    line(rectx,recty,'color','y','linewidth',2); 
+end
+path = 'D:\research\final-year-project\result\result1.jpg';
+saveas(gcf,path)
