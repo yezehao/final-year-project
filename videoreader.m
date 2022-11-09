95fc28cb95473349f862a40f5b9279d1964c4a7c
yezehao <96078570+yezehao@users.noreply.github.com>
2022-11-09 09:04:00 +0000

    move the programme into corresponding

--- videoreader.m
+++
@@ -1,5 +0,0 @@
-%%  Read the video information
-clear; clc;
-obj1 =  ... % input the video to make further process
-VideoReader('D:\research\final-year-project\data\2.5deg_fov\movie_1_1_AA.mp4');
-frame1 = read(obj1,1);
