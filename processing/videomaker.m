ddc121abe847e19aaf40cd6026a9930fe0c9a01a
yezehao <sgzye5@liverpool.ac.uk>
2022-11-15 17:43:24 +0000

    remove useless programme

--- processing/videomaker.m
+++ processing/videomaker.m
@@ -1,11 +1,11 @@
 % write blank video
-provideo=VideoWriter('D:\research\final-year-project\data\preprossess\');
+provideo=VideoWriter('D:\research\final-year-project\result\');
 endFrame = 1495; % The end frames
 provideo.FrameRate = 5; % fps = 5
 open(provideo); % Open file for writing video data
 % imread image from result
 for i= 1:endFrame
-    frames=imread(['D:\research\final-year-project\data\preprossess\',num2str(i),'.jpg']);
+    frames = imread(['D:\research\final-year-project\data\preprossess\',num2str(i),'.jpg']);
     writeVideo(provideo,frames);
 end
 close(provideo);
