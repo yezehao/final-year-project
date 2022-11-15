28f878032a0f2073749895660217294977381cf4
yezehao <sgzye5@liverpool.ac.uk>
2022-11-15 17:42:59 +0000

    modify the programme for video reader

--- processing/videoreader.m
+++ processing/videoreader.m
@@ -6,15 +6,26 @@ nframes = get (videoobj, "NumFrames"); % Get the number of frames
 
 %% Preprocessing
 for i = 1:(nframes-1)
+
     % Read in the frames
     frame_1 = rgb2gray(read(videoobj,i));
     frame_2 = rgb2gray(read(videoobj,i+1));
-    % Use the minimum SE to dilate substracted image
-    I = frame_2-frame_1;
-    SE = strel('diamond',2); 
-    sub = imdilate(I,SE);
+    I = frame_2-frame_1;% get the substration of frames
+
+    % image Processing
+    SE = strel('diamond',2); % construct SE for dilation
+    I_dilation = imdilate(I,SE); % use dilation to expand the object
+    I_pre = imbinarize(I_dilation,0.12); % convert grayscale image into binary image
+    I_binary = imdilate(I_pre,SE);
+    I_pro = bwareaopen(I_binary,5); % delete the obj smaller than 5 pixels
+    [labelpic,num] = bwlabel (I_pro,8); % label the objects
+    
+    % Region Centroid
+    prop = regionprops(labelpic,"Centroid"); % get the centroid coordinates
+    structure(i).centroid = prop; % store the coordinates in the structure
+
     % Display and store the processed images
-    imshow(sub);
-    path = ['D:\research\final-year-project\data\preprossess\',num2str(i),'.jpg'];
-    saveas(gcf,path)
+    % imshow(sub);
+    % path = ['D:\research\final-year-project\data\preprossess\',num2str(i),'.jpg'];
+    % saveas(gcf,path)
 end
