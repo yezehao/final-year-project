44914b3422b913686569a9f7ff111495b39f79f4
yezehao <sgzye5@liverpool.ac.uk>
2022-12-08 09:10:23 +0000

    modify the programme of videomaker

--- processing/videomaker.m
+++ processing/videomaker.m
@@ -14,18 +14,18 @@ for i = 1:numframes-3
         z(2) = coordinate(3*j-1,i);
         plot(z(1), z(2), 'ro');
     end
-    path = ['C:\Users\30348\Documents\final-year-project\result\single-vehicle\' num2str(i) '.jpg'];
+    path = ['C:\Users\30348\Documents\final-year-project\result\vehicle(no-github)\' num2str(i) '.jpg'];
     saveas(gcf,path)
     close;
 end
 % write blank video
-provideo=VideoWriter('C:\Users\30348\Documents\final-year-project\result\single-vehicle-tracking');
+provideo=VideoWriter('C:\Users\30348\Documents\final-year-project\result\vehicletracking(no-github)');
 endFrame = numframes-3; % The end frames
 provideo.FrameRate = 5; % fps = 5
 open(provideo); % Open file for writing video data
 % imread image from result
 for i= 1:endFrame
-    frames = imread(['C:\Users\30348\Documents\final-year-project\result\single-vehicle\',num2str(i),'.jpg']);
+    frames = imread(['C:\Users\30348\Documents\final-year-project\result\vehicle(no-github)\',num2str(i),'.jpg']);
     writeVideo(provideo,frames);
 end
 close(provideo);
