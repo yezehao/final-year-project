5e3abefe2e3c6d750f5892d5406c4c433a2e7336
yezehao <sgzye5@liverpool.ac.uk>
2023-03-03 11:55:29 +0000

    modify the programme about tracking

--- processing/tracking.m
+++ processing/tracking.m
@@ -5,102 +5,52 @@ clear; clc;
 % currentfolder = pwd;
 % cd processing\
 % load([currentfolder,'\data\centroid_v2\2.5_1_1.mat']);
-load("test\test.mat")
-numframes = 1496; % the number of frames is 1495
-[path] = Association(structure,numframes);
+load("test\The-Association-Test\test.mat")
+numframes = 100; % the number of frames is 1495
 
-% %% Save the matrix into authorization file
-% save('association\2.5_1_1.mat','coordinate')
+% Initialize Tracking
+firstrun = [0; 0; 0];
 
-
-%% Distance-based Association With Kalman Filter
-clear; clc;
-load('centroid_v2\2.5_1_1.mat');
-numframes = 1496; % the number of frames is 1495
-
-%% The loop using distance-base association to associate centroid together
-loopnum = 1;
-for i = 1:(numframes-2)
+% Tracking Procedue
+for i = 1:(numframes-1)
     a = structure(i).centroid;
     b = structure(i+1).centroid;
-    if isempty(a) == 0 && isempty(b) ==0 % The two series of centroid cannot be empty
-        % Calculate the distance among the points
-        matrix = pdist2(a,b);
-        [size_x,size_y] = size(matrix);
-        % Find the points corresponding to the minimum value
-        for j = 1:(min(size_x, size_y))
-            [x,y] = find(min(min(matrix)) == matrix); % find the minimum distance
-            if min(min(matrix)) <= 15 % The speed of vehicle should within limitation
-                z = x; z(:,1) = loopnum;
-                path(2*j-1,i) = x(1);
-                path(2*j-0,i) = y(1);
-                loop(1,i) = z(1);
-                % replace the colomn of minimum distance with large number
-                for m = 1:size_x
-                    matrix(m,y(1)) = 10000;end
-                % replace the row of minimum distance with large number
-                for n = 1:size_y    
-                    matrix(x(1),n) = 10000;end
-            else
-                loopnum = loopnum+1;
+    for j = 1:length(a(:,1))
+        if firstrun(j,i) == 0
+            [A,firstrun] = F_Association(a,b,i,firstrun);
+            kalman(j).S = 10^6 * eye(4);
+            kalman(j).X = [0,0,0,0]';
+            association(:,i) = A(:,1);
+            track(i).centroid = a;
+        elseif firstrun(j,i) == 1
+            for k = 1:length(a(:,1))
+                S = kalman(k).S;
+                X = kalman(k).X;
+                m = a(k,:);
+                [mk, S, X] = F_Kalmanfilter(m, S, X);
+                ak(k,:) = mk;
+                kalman(k).S = S;
+                kalman(k).X = X;            
             end
+            [A,firstrun] = F_Association(ak,b,i,firstrun);
+            association(:,i) = A(:,1);
+            track(i).centroid = ak;
         end
     end
 end
 
 % clear the useless variables
-clearvars loopnum a b size_x size_y m n
-clearvars x y z i j k
+clearvars a ak m mk b i j k A S X
 
-%% The association of the single vehicle
-firstrun = 0; % represent the program run for the first time
-trackID = 0; % use to label the vehicles that tracking
-% The initial state matrix
-S = 10^6 * eye(4);
-X = [746,1000,0,0]';
-for i = 1:(numframes-3)
-    if loop(i) ~= 0 && loop(i) == loop(i+1)
-        if firstrun == 0 % The beginning of tracking
-            % The tracking transfromed to another object and the trackID changed
-            trackID = trackID + 1;
-            n = path(1,i);
-            series(trackID).centroid(:,i) = [structure(i).centroid(n,:)]';
-            firstrun = 1;
-            n = path(2,i);
-        elseif firstrun == 1 % The procedue of tracking
-            M = [structure(i).centroid(n,:)]';
-            [xk, S, X, firstrun] = kalmanfilter(M, S, X, firstrun);
-            series(trackID).centroid(:,i) = xk;
-            next = find(path(:,i) == n);
-            if mod(next(1),2) == 1
-                next = next(1);
-            elseif numel(next) == 1
-                firstrun = 0;
-            else
-                next = next(2);
-            end
-            n = path(next+1,i);
-        end
-    elseif loop(i) ~= 0 && loop(i) ~= loop(i+1) % The end of tracking
-        if firstrun == 0
-            % The tracking transfromed to another object and the trackID changed
-            trackID = trackID + 1;
-            n = path(1,i);
-            series(trackID).centroid(:,i) = [structure(i).centroid(n,:)]';
-            firstrun = 0;
-        elseif firstrun == 1
-            M = [structure(i).centroid(n,:)]';
-            [xk, S, X, firstrun] = kalmanfilter(M, S, X, firstrun);
-            series(trackID).centroid(:,i) = xk;
-            firstrun = 0;
-        end
-    end 
+figure,
+for i = 1:numframes-2
+    a = track(i).centroid; b = track(i+1).centroid;
+    c = structure(i).centroid; d = structure(i+1).centroid;
+    line([a(1,1),b(1,1)],[a(1,2),b(1,2)],"color","b")
+    line([a(2,1),b(2,1)],[a(2,2),b(2,2)],"color","b")
+    line([a(3,1),b(3,1)],[a(3,2),b(3,2)],"color","b")
+    line([c(1,1),d(1,1)],[c(1,2),d(1,2)],"color","r")
+    line([c(2,1),d(2,1)],[c(2,2),d(2,2)],"color","r")
+    line([c(3,1),d(3,1)],[c(3,2),d(3,2)],"color","r")
 end
 
-% clear the useless variables
-clearvars firstrun i n next trackID
-clearvars M S X xk
-
-
-%% Save the matrix into authorization file
-save('association\single\2.5_1_1.mat','series')
