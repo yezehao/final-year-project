9ec87636e424bfb4ff678569615fd35167a5cbbf
yezehao <sgzye5@liverpool.ac.uk>
2023-03-03 11:49:17 +0000

    modify the test file about association test

--- processing/test/The-Association-Test/test_data_generation.m
+++ processing/test/The-Association-Test/test_data_generation.m
@@ -1,14 +1,26 @@
 %% test data generation
 clear;clc;
-nframes = 1496;
+nframes = 100;
 for i = 1:nframes
-    centroid(1,1) = 100 + i + (-0.5 + rand(1,1));
-    centroid(1,2) = 100 + i + (-0.5 + rand(1,1));
-    centroid(2,1) = 1000 + i + (-0.5 + rand(1,1));
-    centroid(2,2) = 1000 + i + (-0.5 + rand(1,1));
-    centroid(3,1) = 2000 - i + (-0.5 + rand(1,1));
-    centroid(3,2) = 2000 - i + (-0.5 + rand(1,1));
+    centroid(1,1) = 100  + 10*i + 7.5*(-0.5 + rand(1,1));
+    centroid(1,2) = 100  + 10*i + 7.5*(-0.5 + rand(1,1));
+    centroid(2,1) = 100  + 10*i + 7.5*(-0.5 + rand(1,1));
+    centroid(2,2) = 1100 - 10*i + 7.5*(-0.5 + rand(1,1));
+    centroid(3,1) = 600 +  0*i  + 7.5*(-0.5 + rand(1,1));
+    centroid(3,2) = 100 +  10*i + 7.5*(-0.5 + rand(1,1));
     structure(i).centroid = centroid;
 end
 
-save('test\test.mat','structure')
+figure,
+for i = 1:nframes-1
+    centroid1 = structure(i).centroid;
+    centroid2 = structure(i+1).centroid;
+
+    line([centroid1(1,1),centroid2(1,1)],[centroid1(1,2),centroid2(1,2)],"color","r");
+    line([centroid1(2,1),centroid2(2,1)],[centroid1(2,2),centroid2(2,2)],"color","g");
+    line([centroid1(3,1),centroid2(3,1)],[centroid1(3,2),centroid2(3,2)],"color","b");
+end
+
+
+
+save('test.mat','structure')
