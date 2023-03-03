444cf5209c762bda2e6fdbf796e1c045d6462469
yezehao <sgzye5@liverpool.ac.uk>
2023-03-03 11:54:02 +0000

    modify the centroid finding programme

--- processing/centroid_finding.m
+++ processing/centroid_finding.m
@@ -1,20 +1,18 @@
 %%  Read the video information
 clear; clc;
 % get the path of current folder
-cd ('..');
-currentfolder = pwd;
-cd ('processing\');
+cd ('..'); currentfolder = pwd; cd ('processing\');
 path = [currentfolder,'\data\'];
-% read in the information about video
-for j = 1:12
-    videoobj =  ... 
-    VideoReader([path,'video(no-github)\2.5_2_',num2str(j),'.mp4']);
-    nframes = get (videoobj, "NumFrames"); % Get the number of frames
+
+for j = 9:12
+    % read in the information about video
+    videoobj = VideoReader([path,'video(no-github)\2.5_1_',num2str(j),'.mp4']);
+    nframes = get(videoobj, "NumFrames");
     
     %% Image Processing Version 2
     for i = 1:(nframes-1)
         % Image Processing
-        [centroid] = ImageProcessing(videoobj,i);    
+        [centroid] = F_ImageProcessing(videoobj,i);    
         structure(i).centroid = centroid; % store the coordinates in the structure
         % This is added for knowing the progress of image processing 
         if mod(i,20) == 0
@@ -23,8 +21,8 @@ for j = 1:12
     end
     
     %% Save the structure into data file
-    filename = ['2.5_2_',num2str(j),'.mat'];
-    save([path,'\centroid_v2\',filename],'structure')
+    filename = ['2.5_1_',num2str(j),'.mat'];
+    save([path,'\centroid\',filename],'structure')
     % clear variables
     clearvars structure videoobj
 end
