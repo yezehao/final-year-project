f0445d273fbb17109015b2f73e357681c830cb1b
yezehao <sgzye5@liverpool.ac.uk>
2023-03-23 20:21:26 +0000

    modify the visualization programmes

--- processing/display_detection.m
+++ processing/display_detection.m
@@ -2,10 +2,10 @@
 clear; clc;
 cd ('..'); currentfolder = pwd; cd ('processing\');
 path = [currentfolder,'\data\'];
-for i = 2:12
+for i = 1:12
     % Data Input
-    load([path,'centroid\2.5_1_',num2str(i),'.mat']);
-    videoobj = VideoReader([path,'video(no-github)\2.5_1_',num2str(i),'.mp4']);
+    load([path,'centroid\2.5_2_',num2str(i),'.mat']);
+    videoobj = VideoReader([path,'video(no-github)\2.5_2_',num2str(i),'.mp4']);
     numframes = 1496;
     for j = 1:numframes-1
         centroid = structure(j).centroid;
@@ -18,21 +18,21 @@ for i = 2:12
                 plot(centroid(k,1),centroid(k,2),"or")
             end
         end
-        path_result = [currentfolder,'\result (no-github)\measurement\2.5_1_',num2str(i),'_',num2str(j),'.jpg'];
+        path_result = [currentfolder,'\result (no-github)\measurement\2.5_2_',num2str(i),'_',num2str(j),'.jpg'];
         saveas(gcf,path_result);
         close;
     end
 end
 
 % Video Generation
-for j = 2:12
-provideo=VideoWriter([currentfolder,'\result (no-github)\measurement_2.5_1_',num2str(j),'.avi']);
+for j = 1:12
+provideo=VideoWriter([currentfolder,'\result (no-github)\measurement_2.5_2_',num2str(j),'.avi']);
 endFrame = numframes-1; % The end frames
 provideo.FrameRate = 5; % fps = 5
 open(provideo); % Open file for writing video data
 % imread image from result
 for i= 1:endFrame % endFrame
-    frame_path = [currentfolder,'\result (no-github)\measurement\2.5_1_',num2str(j),'_',num2str(i),'.jpg'];
+    frame_path = [currentfolder,'\result (no-github)\measurement\2.5_2_',num2str(j),'_',num2str(i),'.jpg'];
     frames = imread(frame_path);
     writeVideo(provideo,frames);
 end
--- processing/display_detection_with_1.m
+++ processing/display_detection_with_1.m
@@ -3,7 +3,7 @@ clear; clc;
 cd ('..'); currentfolder = pwd; cd ('processing\');
 path = [currentfolder,'\data\'];
 % Data Input
-load([path,'track\2.5_1.mat']);
+load([path,'track\2.5_2.mat']);
 
 numframes = 1496;
 frame_l = 2000; % The length of frame
@@ -45,69 +45,83 @@ for t = 1:length(Track)
     end
 end
 
-% clear variables
-clearvars a b C firstrun  i loc_video loc_video1 loc_video2 t 
+% % clear variables
+% clearvars a b C firstrun  i loc_video loc_video1 loc_video2 t 
+% 
+% % Calculate the length of tracks
+% for i = 1:length(traj)
+%     a = traj(i).frame;
+%     for j = 1:length(a(:,1))-2
+%         traj(i).L(j,1) = a(j+1,1)-a(j,1);
+%     end
+% end
+% 
+% for i = 1:length(traj)
+%     if isempty(traj(i).L) == 0
+%         MAX(i,1) = max(traj(i).L);
+%     else
+%         MAX(i,1) = 0;
+%     end
+% end
 
-for i = 1:length(traj)
-    a = traj(i).frame;
-    for j = 1:length(a(:,1))-2
-        traj(i).L(j,1) = a(j+1,1)-a(j,1);
-    end
+Nt = 158; % Choose the track whose ID is 3674
+a = [0,0,1; 1,0,1; 2,0,1; 3,0,1; 4,0,1; 5,0,1;
+     0,1,1; 1,1,1; 2,1,1; 3,1,1; 4,1,1; 5,1,1;];
+
+T = traj(Nt).frame; 
+T_sp = Track(Nt).centroid;
+for i = 1:length(T(:,1))-2
+    frame(i,1) = T(i,1)+1;
+    frame(i,2) = T(i+1,1);
+    loc_video(i,1) = T(i+1,2);
 end
 
-for i = 1:length(traj)
-    if isempty(traj(i).L) == 0
-        MAX(i,1) = max(traj(i).L);
-    else
-        MAX(i,1) = 0;
+for k = 1:length(frame)
+    loc = loc_video(k,1);
+    % Read in the centroid data and imagery for further processing
+    videoobj = VideoReader([path,'video(no-github)\2.5_2_',num2str(loc),'.mp4']);
+    load([currentfolder,'\data\centroid\2.5_2_',num2str(loc),]);
+    
+    for i = frame(k,1):frame(k,2)-1
+        video_frame =read(videoobj,i);
+        C = structure(i).centroid;
+        figure, 
+        imshow(video_frame); % Plot the background
+        hold on
+
+        % Plot all the detections
+        for j = 1:length(C(:,1))
+            plot(C(j,1),C(j,2),"or")
+        end
+
+        % Plot the Track of specific car
+        for j = frame(k,1):frame(k,2)-1 
+            sub = [frame_w * a(loc,1), frame_l * a(loc,2)]; % need to be modify if the video changed
+            L = [T_sp(j,1), T_sp(j,2); T_sp(j+1,1), T_sp(j+1,2)] - [sub;sub];
+            line([L(1),L(2)],[L(3),L(4)],"color","blue")
+        end
+        % Plot the location of the specific car
+        sub = [frame_w * a(loc,1), frame_l * a(loc,2)]; % need to be modify if the video changed
+        loca = [T_sp(i,1), T_sp(i,2)] - sub;
+        plot(loca(1,1),loca(1,2),"*m")
+    
+        path_result = [currentfolder,'\result (no-github)\measurement with track ',num2str(Nt),'\',num2str(i),'.jpg'];
+        saveas(gcf,path_result);
+        close;
     end
-end
-
-[max1,max2] = find(MAX==max(MAX));
-frame_t = traj(max1).frame; 
-T_sp = Track(max1).centroid;
-frame1 = frame_t(1,1)+1;
-frame2 = frame_t(2,1);
-loc_video = frame_t(2,2);
-
-videoobj = VideoReader([path,'video(no-github)\2.5_1_',num2str(loc_video),'.mp4']);
-load([currentfolder,'\data\centroid\2.5_1_',num2str(loc_video),]);
-
-for i = frame1:frame2
-    frame =read(videoobj,i);
-    C = structure(i).centroid;
-    figure, 
-    imshow(frame); % Plot the background
-    hold on
-    % Plot all the detections
-    for j = 1:length(C(:,1))
-        plot(C(j,1),C(j,2),"or")
-    end
-    % Plot the Track of specific car
-    for k = frame1:frame2-1 
-        sub = [frame_w * 0, frame_l * 0]; % need to be modify if the video changed
-        L = [T_sp(k,1), T_sp(k,2); T_sp(k+1,1), T_sp(k+1,2)] - [sub;sub];
-        line([L(1),L(2)],[L(3),L(4)],"color","blue")
-    end
-    % Plot the location of the specific car
-    sub = [frame_w * 0, frame_l * 0]; % need to be modify if the video changed
-    loca = [T_sp(i,1), T_sp(i,2)] - sub;
-    plot(loca(1,1),loca(1,2),"*m")
-
-    path_result = [currentfolder,'\result (no-github)\measurement with 1 car\',num2str(i),'.jpg'];
-    saveas(gcf,path_result);
-    close;
 end
 
 % Video Generation
-provideo=VideoWriter([currentfolder,'\result (no-github)\measurement with 1 car.avi']);
+provideo=VideoWriter([currentfolder,'\result (no-github)\measurement with track ',num2str(Nt),'.avi']);
 endFrame = numframes-1; % The end frames
 provideo.FrameRate = 5; % fps = 5
 open(provideo); % Open file for writing video data
 % imread image from result
-for i= frame1:frame2 % endFrame
-    frame_path = [currentfolder,'\result (no-github)\measurement with 1 car\',num2str(i),'.jpg'];
+for k = 1:length(loc_video)
+for i= frame(k,1):frame(k,2)-2 % endFrame
+    frame_path = [currentfolder,'\result (no-github)\measurement with track ',num2str(Nt),'\',num2str(i),'.jpg'];
     frames = imread(frame_path);
     writeVideo(provideo,frames);
+end
 end
 close(provideo);
